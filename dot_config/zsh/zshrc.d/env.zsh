18543c2e9f16655bfcbd80b6181d7e1fb11b02c6
Abraham Murciano <abraham.murciano@mobileye.com>
2025-08-13 12:03:30 +0300

    Add .config/tmux/scripts/toggle-split-direction Update .config/tmux/tmux.conf Update .config/zsh/zshrc.d/env.zsh

--- dot_config/zsh/zshrc.d/env.zsh
+++ dot_config/zsh/zshrc.d/env.zsh
@@ -1,4 +1,7 @@
 export EDITOR='nano'
+
 export PAGER='less'
+export LESS='--mouse --wheel-lines=15 --search-skip-screen --ignore-case --RAW-CONTROL-CHARS --tabs=4'
+
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.local/lib"
 export JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION=1
